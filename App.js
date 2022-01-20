b77d2a8c3476b644e1bb3eefac0d3f6222800d47
sudeekshaganguli <72445242+sudeekshaganguli@users.noreply.github.com>
2022-01-20 20:42:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { StyleSheet,SafeAreaView,ImageBackground,StatusBar } from "react-native";
+
+import Signin from "./src/Signin";
+
+export default function App() {
+  return (
+        
+    <Signin visible={true}/>
+
+
+   
+  );
+}
+
+
+
+
+
+
+ 
+
+
+
